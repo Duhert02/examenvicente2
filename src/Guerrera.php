c7a36d5e729511165c2f93d3dba3dbc19939701d
Duhert02 <81563377+Duhert02@users.noreply.github.com>
2022-02-02 12:28:42 +0100

    Update Guerrera.php

--- src/Guerrera.php
+++ src/Guerrera.php
@@ -5,7 +5,12 @@ class Guerrera
     public $vida; #Valor de la vida que posee
     public $estado; #La guerrera estará 'viva' o 'muerta', dependiendo del valor de su vida. >0 = viva; <=0 = muerta
 
-    public function __construct(){}
+    public function __construct($a1,$a2,$a3){
+
+        $this->nombre=$a1;
+        $this->vida=$a2;
+        $this->estado=$a3;
+    }
 
 
     public function herida($vida,$estado){
