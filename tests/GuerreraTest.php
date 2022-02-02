2b83ff7014327ea5495803a9c29365651c09f669
Duhert02 <81563377+Duhert02@users.noreply.github.com>
2022-02-02 12:19:49 +0100

    Update GuerreraTest.php

--- tests/GuerreraTest.php
+++ tests/GuerreraTest.php
@@ -11,20 +11,22 @@ class TestGuerrea extends TestCase{
 
     public function testHeridaAlive(){
         $this->assertEquals('alive',$this->op->herida(11,'alive'));
-    
+        return $this->estado;
     }
 
     public function testHeridaDead(){
         $this->assertEquals('dead',$this->op->herida(9,'alive'));
-             
+       return $this->estado;
     }
 
     public function testPocionViva(){
         $this->assertEquals('dead',$this->op->pocion(-11,'dead'));
+       return $this->estado;
     }
 
     public function testPocionMuerta(){
         $this->assertEquals('alive',$this->op->pocion(-9,'dead'));
+        return $this->estado;
     }
 
 }
